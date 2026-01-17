df85714ac5b0ad890349c2cf6a1eb969859c7b2b
Tom Hodder <tom@limepepper.co.uk>
2026-01-17 14:41:06 +0000

    checkpoint
    # Conflicts:
    #	src/main/java/org/limepepper/mqttbot/integrations/baritone/BaritoneGotoHandler.java

---
+++ src-py/mqttbot/core/state/game_module.py
@@ -0,0 +1,16 @@
+from mqttbot import MessageData
+from mqttbot.core.state.game_state import GameState
+from mqttbot.core.state.state_module import StateModule
+
+
+class GameModule(StateModule[GameState]):
+    def __init__(self):
+        self._state = GameState()
+
+    def handle_event(self, event: MessageData) -> None: ...
+
+    def get_state(self) -> GameState:
+        return self._state
+
+    def reset_state(self) -> None:
+        self._state = GameState()
